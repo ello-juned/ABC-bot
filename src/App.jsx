588cb7162628a83c8f28d97a35e0c71e7e788033
ellojuned <ello.juned009@gmail.com>
2023-10-13 12:26:21 +0530

    ui updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ function App() {
 
   return (
     <>
-      <div className="w-screen h-screen flex items-center justify-center">
-        <div className="p-8 bg-white rounded-lg shadow-md">
-          <div className="text-center mt-4">
+      <div className="w-screen h-screen flex items-center justify-center bg-black text-white">
+        <div className="p-8  shadow-2xl  justify-center items-center tx  shadow-white min-h-[60%] min-w-[50%] rounded-2xl">
+          <div className="text-center mt-4 flex flex-col items-center justify-center">
             <p className="text-lg font-semibold">Property for Sale or Rent</p>
             <p className="text-gray-500">
               This beautiful property is available for both sale and rent. It
